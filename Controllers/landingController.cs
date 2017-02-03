1b727a074bbbf1e40d090d61af604a085e8ae687
mdjibran <mohdjibran316@gmail.com>
2017-02-03 12:06:21 -0500

    Code Generator

---
+++ Controllers/landingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codeGenerator.Controllers
+{
+    public class landingController : Controller
+    {
+        //
+        // GET: /landing/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="codeGenerator.MvcApplication" Language="C#" %>
